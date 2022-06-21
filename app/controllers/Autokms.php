5adc329d3a3141bab98cf099d97017d49096fcba
Mahircan Alkan <328827@student.mboutrecht.nl>
2022-06-21 14:31:30 +0200

    update set and ready
    update function build
    
    now only button

--- app/controllers/Autokms.php
+++ app/controllers/Autokms.php
@@ -13,7 +13,7 @@ class Autokms extends Controller
     public function index()
     {
 
-        $model = $this->kilometerModel->GetSingleInvoerAuto();
+        $model = $this->kilometerModel->GetAllInvoerAuto();
         $tablesRow = "";
         foreach ($model as $value) {
             $tablesRow .= " 
@@ -36,10 +36,81 @@ class Autokms extends Controller
             'data' => $tablesRow
         ];
 
-        // $this->view('indexes/read', $data);
-
-
-
         $this->view('invoeren/index', $data);
+    }
+
+
+
+    public function validate($values = [])
+    {
+        $validate = true;
+        foreach ($values as $value) {
+            if (empty($_POST[$value]))
+                $validate = false;
+            break;
+        }
+        return $validate;
+    }
+
+
+
+    public function kmInvoeren($id = null)
+    {
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            if (!$this->validate(['kmstand'])) {
+
+                echo "Het veranderen van de kmstand is niet gelukt";
+                header("Refresh:2; url=" . URLROOT .  "/Autokm/index/creating-failed");
+            } else {
+                try {
+
+                    filter_input_array(INPUT_POST, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+                    $this->kilometerModel->updateKmStand($_POST);
+                    header("Location: " . URLROOT . "./Autokm/index/update-succes");
+                } catch (PDOException $e) {
+                    header("Location: " . URLROOT . "./overzicht/index/update-failed");
+                }
+            }
+        } else {
+
+            try {
+
+                $row = $this->kilometerModel->getSingleKm($id);
+
+                if (!empty($row)) {
+
+                    // function voor fillselector die je oproept om hierin te werken
+                    $records = $this->kmSelector($row->kmstand);
+                } else {
+
+                    header("Location: " . URLROOT . "./Autokm/index");
+                }
+            } catch (PDOException $e) {
+
+                echo $e->getMessage();
+                header("Location: " . URLROOT . "./Autokm/index");
+            }
+            $data = [
+                'title' => "<h1>Update Km</h1>",
+                'row' => $row,
+                'records' => $records
+
+            ];
+
+            $this->view("invoeren/update", $data);
+        }
+    }
+
+
+
+    public function kmSelector($info = '')
+    {
+        $records = "";
+        foreach ($this->model("Autokm")->getSingleKm() as $record) {
+            $selected = ($info == $record->Auto_Model) ? "selected" : ""; //check if the category is the one we have selected
+            $records .= "<option value = '" . $record->Auto_Model . "'" . $selected . ">" . $record->Auto_Model .  "</option>";
+        }
+        return $records;
     }
 }
--- app/models/Autokm.php
+++ app/models/Autokm.php
@@ -9,13 +9,31 @@ class Autokm
         $this->db = new Database;
     }
 
-    public function GetSingleInvoerAuto()
+    public function GetAllInvoerAuto()
     {
 
-        $this->db->query('SELECT * FROM kilometersinvoer ORDER BY id');
+        $this->db->query('SELECT * FROM kmstanden ORDER BY id');
         $this->db->execute();
         return $this->db->resultSet();
     }
 
-    
+    // get single om enkel km te roepen
+    public function getSingleKm($id)
+    {
+        $this->db->query("SELECT * FROM kmstanden WHERE id = :id");
+        $this->db->bind(':id', $id, PDO::PARAM_INT);
+        return  $this->db->single();
+    }
+
+
+    public function updateKmStand($post)
+    {
+        $this->db->query("UPDATE kmstanden
+                        SET kmstand = :kmstand
+                        WHERE id = :id");
+
+        $this->db->bind(':kmstand', $post["kmstand"], PDO::PARAM_INT);
+
+        return $this->db->execute();
+    }
 }
